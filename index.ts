6f3c912179a68b97ffecd6a656727f0676f90ec8
Do-Repo <>
2022-11-28 19:57:35 +0100

    Minor changes

--- index.ts
+++ index.ts
@@ -47,7 +47,7 @@ app.all('*', (req: Request, res: Response, next: NextFunction) => {
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   err.status = err.status || 'error';
   err.statusCode = err.statusCode || 500;
-
+  console.log(err.message)
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -23,16 +23,16 @@ export const updateProfileHandler = async (
     res: Response,
     next: NextFunction
 ) => {
+
     try {
         const user = await updateUser(res.locals.user._id, req.body)
         res.status(200).json({
             status: 'success',
-            result: {
-                user,
-            },
+            user,  
         })
     }catch(err: any){
         next(err);
+        console.log(err)
     }
 }
     
